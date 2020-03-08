39d484063d04c487bdf84621314f0d3000b61162
Robert Hambrock <roberthambrock@gmail.com>
2020-03-08 17:10:26 +0200

    Integrate parity-scale-codec for testing global consistency
    Via FFI, pass byte literal string to the SCALE implementation
    `parity-scale-codec` (Rust). This then decodes the data based on specified
    type (currently hardcoded).

--- spec/ffi_spec.rb
+++ spec/ffi_spec.rb
@@ -1,27 +1,25 @@
 # coding: utf-8
 
 require 'scale'
+require_relative './types_for_test.rb'
 require 'ffi'
 
 module Rust
   extend FFI::Library
   ffi_lib 'target/debug/libvector_ffi.' + FFI::Platform::LIBSUFFIX
-  attach_function :vector_input, %i[pointer int], :int
+  attach_function :byte_string_literal_parse, %i[pointer int], :bool
 end
 
-# vec = [7, 8, 9]
-# vec_c = FFI::MemoryPointer.new(:int, vec.size)
-# vec_c.write_array_of_int vec
-# describe do
-#   it 'should return 1 regardless of input' do
-#     expect(Rust.vector_input(vec_c, vec.size)).to eql(1)
-#   end
-# end
+encoded_u64 = Scale::Types::U64.new(14_294_967_296).encode
+describe do
+  it 'encoding should match specification' do
+    expect(encoded_u64).to eql('00e40b5403000000')
+  end
+end
+vec = Scale::Bytes.new('0x' + encoded_u64).bytes
 
-scale_bytes = Scale::Bytes.new('0x45')
-# scale_bytes = Scale::Bytes.new('0x0c003afe')
-scale_bytes = Scale::Bytes.new('0x2a00')
-puts scale_bytes.bytes
-scale_bytes_c = FFI::MemoryPointer.new(48)
-scale_bytes_c.write_array_of_int scale_bytes.bytes
-Rust.vector_input(scale_bytes_c, 12)
+FFI::MemoryPointer.new(:int8, vec.size) do |vec_c|
+  vec_c.write_array_of_int8 vec
+  puts "vec_c: #{ vec_c }"
+  puts Rust.byte_string_literal_parse(vec_c, vec_c.size)
+end
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,19 @@
+extern crate parity_scale_codec;
 use std::slice;
+use parity_scale_codec::{Encode, Decode};
 
 #[no_mangle]
-pub extern fn vector_input(v_pointer: *const u8, len: usize) -> u32 {
-    println!("pointer is {:?}", v_pointer);
+pub extern fn byte_string_literal_parse(v_pointer: *const u8, len: usize) -> bool {
     let data_slice = unsafe {
         assert!(!v_pointer.is_null());
         slice::from_raw_parts(v_pointer, len)
     };
     let v = data_slice.to_vec();
     println!("vector is {:?}", v);
-    1
+    v.using_encoded(|ref slice| {
+        println!("encoded slice: {:?}", slice);
+    });
+    println!("{:?}", <u64>::decode(&mut &v[..]).unwrap());
+    true
 }
+
