b78e03e6e11b6c23fbc0350db74d16b6e70169f6
Robert Hambrock <roberthambrock@gmail.com>
2020-03-08 18:28:45 +0200

    Refactor ruby ffi encoder to make generic
    In preparation to add further types that can be encoded for the Rust FFI

--- spec/ffi_spec.rb
+++ spec/ffi_spec.rb
@@ -10,23 +10,31 @@ module Rust
   attach_function :byte_string_literal_parse_u64, %i[pointer int uint64], :bool
 end
 
-value = 14_294_967_296
-encoded_u64 = Scale::Types::U64.new(value).encode
-describe do
-  it 'encoding should match specification' do
-    expect(encoded_u64).to eql('00e40b5403000000')
-  end
-end
-vec = Scale::Bytes.new('0x' + encoded_u64).bytes
-puts vec
+parse_u64 = proc { |vec_c, value|
+  Rust.byte_string_literal_parse_u64(vec_c, vec_c.size, value)
+}
 
-FFI::MemoryPointer.new(:int8, vec.size) do |vec_c|
-  vec_c.write_array_of_int8 vec
-  puts "vec_c: #{vec_c}"
-  ffi_success = Rust.byte_string_literal_parse_u64(vec_c, vec_c.size, value)
+def parse_via_ffi(value, encoding, ffi_function, expectation)
+  encoded = encoding.new(value).encode
+  puts encoded
   describe do
-    it 'Rust implementation should decode to expected value' do
-      expect(ffi_success).to eql(true)
+    it 'encoding should match specification' do
+      expect(encoded).to eql(expectation)
+    end
+  end
+  vec = Scale::Bytes.new('0x' + encoded).bytes
+  puts vec
+
+  FFI::MemoryPointer.new(:int8, vec.size) do |vec_c|
+    vec_c.write_array_of_int8 vec
+    puts "vec_c: #{vec_c}"
+    ffi_success = ffi_function.call(vec_c, value)
+    describe do
+      it 'Rust implementation should decode to expected value' do
+        expect(ffi_success).to eql(true)
+      end
     end
   end
 end
+
+parse_via_ffi(14_294_967_296, Scale::Types::U64, parse_u64, '00e40b5403000000')
