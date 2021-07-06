365bf782794b4fe36831624ee17aba8c0c03a0c5
Aki Wu <wuminzhe@gmail.com>
2021-07-06 17:54:53 +0800

    add to_do

--- lib/type_registry.rb
+++ lib/type_registry.rb
@@ -25,6 +25,7 @@ module Scale
           spec_types, @versioning, @spec_version = load_chain_spec_types(spec_name)
           @types = default_types.merge(spec_types)
         rescue => ex
+          # TODO: check different errors
           puts "There is no types json file named #{spec_name}"
           @types = default_types
         end
